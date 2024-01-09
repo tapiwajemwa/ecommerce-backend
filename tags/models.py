6be6e1ad9b853755119e9200c43d25cc62f84bf7
Tapiwa Jemwa <jemwajoshua@gmail.com>
2024-01-09 21:46:16 +0200

    made fixes

--- tags/models.py
+++ tags/models.py
@@ -7,7 +7,7 @@ class Tag(models.Model):
     label = models.CharField(max_length=255)
     
 class TaggedItem(models.Model):
-    tag = models.models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
