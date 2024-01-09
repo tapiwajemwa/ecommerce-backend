bc24dca249ba709988e433798cadb9853279e5e2
Tapiwa Jemwa <jemwajoshua@gmail.com>
2024-01-09 21:12:14 +0200

    New App named likes added to the API

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Tag(models.Model):
@@ -6,3 +8,7 @@ class Tag(models.Model):
     
 class TaggedItem(models.Model):
     tag = models.models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    
